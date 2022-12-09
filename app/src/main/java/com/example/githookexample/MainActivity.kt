bab447d3c54b512bfdf9a89a331a147c0e14f5c9
Yusuf Okur <yusuf.okur@innovance.com.tr>
2022-12-09 23:15:46 +0300

    Test

--- app/src/main/java/com/example/githookexample/MainActivity.kt
+++ app/src/main/java/com/example/githookexample/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() { // asd
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)//testasd
+        super.onCreate(savedInstanceState)//testasdasd
         setContentView(R.layout.activity_main)
     }
 }
